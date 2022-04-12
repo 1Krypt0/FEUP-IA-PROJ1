601f4555a0714ec0c05be879d494ce092594100e
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-04-12 18:15:40 +0100

    Add Return types to heuristics

--- src/heuristics.py
+++ src/heuristics.py
@@ -2,13 +2,13 @@ from state import BoardState
 from math import sqrt
 
 
-def manhattan_distance(node: BoardState):
+def manhattan_distance(node: BoardState) -> int:
     return abs(node.x - node.goal_x) + abs(node.y - node.goal_y)
 
 
-def visited_l(node: BoardState):
+def visited_l(node: BoardState) -> int:
     return len(node.board.all_shapes.difference(node.board.visited_shapes))
 
 
-def euclidian_distance(node: BoardState):
-    return sqrt(((node.goal_x - node.x) ** 2) + ((node.goal_y - node.y) ** 2))
+def euclidian_distance(node: BoardState) -> float:
+    return int(sqrt(((node.goal_x - node.x) ** 2) + ((node.goal_y - node.y) ** 2)))
